a22e09cec00e4d8b192ad7dae12bda92f20b693c
Vasiliy <vash050@rambler.ru>
2020-10-24 18:05:34 +0300

    two

--- my_work/geekshop/geekshop/settings.py
+++ my_work/geekshop/geekshop/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '(9ttj0eyjunbp=$o(9ikb4gk2mg_7v2z^c1@xdex+_wf9-0xpj'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'mainapp',
 ]
 
 MIDDLEWARE = [
