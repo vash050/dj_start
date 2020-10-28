802fd515ceb6d6bb3e27fc4f3eef79a68a87ef13
Vasiliy <vash050@rambler.ru>
2020-10-28 12:45:41 +0300

    - add base - templates - static

--- my_work/geekshop/mainapp/views.py
+++ my_work/geekshop/mainapp/views.py
@@ -2,12 +2,21 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'mainapp/index.html')
+    context = {
+        'page_title': 'главная',
+    }
+    return render(request, 'mainapp/index.html', context=context)
 
 
 def product(request):
-    return render(request, 'mainapp/product.html')
+    context = {
+        'page_title': 'каталог',
+    }
+    return render(request, 'mainapp/product.html', context=context)
 
 
 def contact(request):
-    return render(request, 'mainapp/contact.html')
+    context = {
+        'page_title': 'контакты',
+    }
+    return render(request, 'mainapp/contact.html', context=context)
