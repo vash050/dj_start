f1d92e4dc9a2fb67104c3111a7e523962dffae0e
Vasiliy <vash050@rambler.ru>
2020-10-28 18:20:56 +0300

    - add base - templates - static - menu - contact

--- my_work/geekshop/mainapp/views.py
+++ my_work/geekshop/mainapp/views.py
@@ -16,7 +16,29 @@ def product(request):
 
 
 def contact(request):
+    locations = [
+        {
+            'city': 'Москва',
+            'phone': '8(954) 145-45-85',
+            'mail': 'moskow@bookshoop.ru',
+            'address': 'ул. Ленина 45',
+        },
+        {
+            'city': 'Санкт-Петербург',
+            'phone': '8(812)121-21-21',
+            'mail': 'spb@bookshoop.ru',
+            'address': 'пр. Невский 12',
+        },
+        {
+            'city': 'Барнаул',
+            'phone': '8(365) 194-34-12',
+            'mail': 'barnaul@bookshoop.ru',
+            'address': 'ул. Тракторная 18',
+        },
+
+    ]
     context = {
         'page_title': 'контакты',
+        'locations': locations,
     }
     return render(request, 'mainapp/contact.html', context=context)
