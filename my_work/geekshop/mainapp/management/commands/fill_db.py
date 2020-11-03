17ee06089bd86c2e49ef6d1f66348d5233803ba8
Vasiliy <vash050@rambler.ru>
2020-11-03 19:35:50 +0300

    - management + json done

--- my_work/geekshop/mainapp/management/commands/fill_db.py
+++ my_work/geekshop/mainapp/management/commands/fill_db.py
@@ -3,7 +3,7 @@ import json
 from django.contrib.auth.models import User
 from django.core.management.base import BaseCommand, CommandError
 
-from mainapp.models import GenreBook, Book
+from mainapp.models import GenreBooks, Book
 
 
 def load_from_json(file_name):
@@ -12,18 +12,18 @@ def load_from_json(file_name):
 
 
 class Command(BaseCommand):
-    # help = 'Fill data in database'
-    #
-    # def handle(self, *args, **options):
-    #     items = load_from_json('')  # путь к файлу
-    #     for item in items:
-    #         GenreBook.objects.create(**item)
-    #
-    #     items = load_from_json('')  # путь к файлу
-    #     for item in items:
-    #         genre = GenreBook.objects.get(name=item[''])
-    #         item[''] = genre
-    #         Book.objects.create(**item)
+    help = 'Fill data in database'
 
-    if not User.objects.filter(username='django').exists():
-        User.objects.create_superuser('django', 'django@gb.local', 'geekbrains')
+    def handle(self, *args, **options):
+        items = load_from_json('mainapp/json/genre.json')  # путь к файлу
+        for item in items:
+            GenreBooks.objects.create(**item)
+
+        items = load_from_json('mainapp/json/book.json')  # путь к файлу
+        for item in items:
+            genre = GenreBooks.objects.get(id=item['genre_book'])
+            item['genre_book'] = genre
+            Book.objects.create(**item)
+
+        if not User.objects.filter(username='django').exists():
+            User.objects.create_superuser('django', 'django@gb.local', 'geekbrains')
