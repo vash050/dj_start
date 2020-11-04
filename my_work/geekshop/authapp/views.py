5cb696299504b3d0447dab077656fb6c0f2e5b76
Vasiliy <vash050@rambler.ru>
2020-11-04 17:08:00 +0300

    - management + json done - authapp - css auth_page -logout

--- my_work/geekshop/authapp/views.py
+++ my_work/geekshop/authapp/views.py
@@ -1,11 +1,30 @@
+
+from django.contrib import auth
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
 from authapp.forms import ShopUserLoginForm
+from django.urls import reverse
 
 
 def login(request):
-    form = ShopUserLoginForm()
+    if request.method == 'POST':
+        form = ShopUserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+            user = auth.authenticate(username=username, password=password)
+            if user and user.is_active:
+                auth.login(request, user)
+                return HttpResponseRedirect(reverse('main:index'))
+    else:
+        form = ShopUserLoginForm()
     context = {
         'form': form,
     }
     return render(request, 'authapp/login.html', context)
+
+
+def logout(request):
+    auth.logout(request)
+    return HttpResponseRedirect(reverse('main:index'))
