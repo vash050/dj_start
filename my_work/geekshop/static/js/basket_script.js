d43cb0b2474107e30f1e6876aa7219bd0e368630
Vasiliy <vash050@rambler.ru>
2020-11-14 12:39:01 +0300

    - dz_6

--- my_work/geekshop/static/js/basket_script.js
+++ my_work/geekshop/static/js/basket_script.js
@@ -2,7 +2,15 @@
 
 window.onload = function () {
     console.log("DOM ready");
-    $('.product_price').on('click', function (event) {
-        console.log("target:" + event.target);
+//    $('.basket_record').on('click', '.product_price', function (event) {
+//        console.log("target:", event.target);
+//    })
+    let categoryNames = document.querySelectorAll('input[type=number]');
+    categoryNames.forEach(function (item) {
+       item.onchange = function (event) {
+       let qty = event.target.value
+       let pk = event.target.name
+       console.log("qty:", qty, ", pk" , pk);
+       }
     })
 }
