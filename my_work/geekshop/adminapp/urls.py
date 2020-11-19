aaf6ddd1adbc62d344cfd47794ed78f36a9e7839
Vasiliy <vash050@rambler.ru>
2020-11-19 20:40:43 +0300

    - create user - CBV

--- my_work/geekshop/adminapp/urls.py
+++ my_work/geekshop/adminapp/urls.py
@@ -5,10 +5,14 @@ import adminapp.views as adminapp
 app_name = 'adminapp'
 
 urlpatterns = [
-    path('', adminapp.index, name='index'),
-    path('user/create/', adminapp.user_create, name='user_create'),
-    path('user/delete/<int:user_pk>/', adminapp.user_delete, name='user_delete'),
-    path('user/update/<int:user_pk>/', adminapp.user_update, name='user_update'),
+    # path('', adminapp.index, name='index'),
+    path('', adminapp.ShopUserList.as_view(), name='index'),
+    # path('user/create/', adminapp.user_create, name='user_create'),
+    path('user/create/', adminapp.ShopUseCreateView.as_view(), name='user_create'),
+    # path('user/update/<int:user_pk>/', adminapp.user_update, name='user_update'),
+    path('user/update/<int:pk>/', adminapp.ShopUserUpdateView.as_view(), name='user_update'),
+    # path('user/delete/<int:user_pk>/', adminapp.user_delete, name='user_delete'),
+    path('user/delete/<int:pk>/', adminapp.ShopUserDeleteView.as_view(), name='user_delete'),
 
     path('categories/', adminapp.categories, name='categories'),
 ]
--- my_work/geekshop/adminapp/views.py
+++ my_work/geekshop/adminapp/views.py
@@ -2,72 +2,100 @@ from django.contrib.auth import get_user_model
 from django.contrib.auth.decorators import user_passes_test
 from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
 
 from adminapp.forms import AdminShopUserCreateForm, AdminShopUserUpdateForm
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 
 from mainapp.models import GenreBooks
 
 
-@user_passes_test(lambda x: x.is_superuser)
-def user_create(request):
-    if request.method == 'POST':
-        user_form = AdminShopUserCreateForm(request.POST, request.FILES)
-        if user_form.is_valid():
-            user_form.save()
-            return HttpResponseRedirect(reverse('adminapp:index'))
-    else:
-        user_form = AdminShopUserCreateForm()
-
-    context = {
-        'page_title': 'админка/пользователи/создание',
-        'form': user_form
-    }
-    return render(request, 'adminapp/user_update.html', context=context)
-
-
-@user_passes_test(lambda x: x.is_superuser)
-def index(request):
-    users = get_user_model().objects.all()
-    context = {
-        'page_title': 'админка/пользователи',
-        'object_list': users,
-    }
-    return render(request, 'adminapp/index.html', context=context)
+# @user_passes_test(lambda x: x.is_superuser)
+# def user_create(request):
+#     if request.method == 'POST':
+#         user_form = AdminShopUserCreateForm(request.POST, request.FILES)
+#         if user_form.is_valid():
+#             user_form.save()
+#             return HttpResponseRedirect(reverse('adminapp:index'))
+#     else:
+#         user_form = AdminShopUserCreateForm()
+#
+#     context = {
+#         'page_title': 'админка/пользователи/создание',
+#         'form': user_form
+#     }
+#     return render(request, 'adminapp/user_update.html', context=context)
 
 
-@user_passes_test(lambda x: x.is_superuser)
-def user_update(request, user_pk):
-    user = get_object_or_404(get_user_model(), pk=user_pk)
-    if request.method == 'POST':
-        user_form = AdminShopUserUpdateForm(request.POST, request.FILES, instance=user)
-        if user_form.is_valid():
-            user_form.save()
-            return HttpResponseRedirect(reverse('adminapp:index'))
-    else:
-        user_form = AdminShopUserUpdateForm(instance=user)
+# @user_passes_test(lambda x: x.is_superuser)
+# def index(request):
+#     users = get_user_model().objects.all()
+#     context = {
+#         'page_title': 'админка/пользователи',
+#         'object_list': users,
+#     }
+#     return render(request, 'adminapp/index.html', context=context)
 
-    context = {
-        'page_title': 'админка/пользователи/редактирование',
-        'form': user_form
-    }
-    return render(request, 'adminapp/user_update.html', context=context)
+class ShopUserList(ListView):
+    model = get_user_model()
 
 
-@user_passes_test(lambda x: x.is_superuser)
-def user_delete(request, user_pk):
-    user = get_object_or_404(get_user_model(), pk=user_pk)
-    if not user.is_active or request.method == 'POST':
-        if user.is_active:
-            user.is_active = False
-            user.save()
-        return HttpResponseRedirect(reverse('adminapp:index'))
+class ShopUseCreateView(CreateView):
+    model = get_user_model()
+    form_class = AdminShopUserCreateForm
+    success_url = reverse_lazy('adminapp:index')
 
-    context = {
-        'page_title': 'админка/пользователи/удаление',
-        'object': user
-    }
-    return render(request, 'adminapp/user_delete.html', context=context)
+
+class ShopUserUpdateView(UpdateView):
+    model = get_user_model()
+    form_class = AdminShopUserUpdateForm
+    success_url = reverse_lazy('adminapp:index')
+
+
+class ShopUserDeleteView(DeleteView):
+    model = get_user_model()
+    success_url = reverse_lazy('adminapp:index')
+
+    def delete(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        success_url = self.get_success_url()
+        self.object.is_active = False
+        self.object.save()
+        return HttpResponseRedirect(success_url)
+
+
+# @user_passes_test(lambda x: x.is_superuser)
+# def user_update(request, user_pk):
+#     user = get_object_or_404(get_user_model(), pk=user_pk)
+#     if request.method == 'POST':
+#         user_form = AdminShopUserUpdateForm(request.POST, request.FILES, instance=user)
+#         if user_form.is_valid():
+#             user_form.save()
+#             return HttpResponseRedirect(reverse('adminapp:index'))
+#     else:
+#         user_form = AdminShopUserUpdateForm(instance=user)
+#
+#     context = {
+#         'page_title': 'админка/пользователи/редактирование',
+#         'form': user_form
+#     }
+#     return render(request, 'adminapp/user_update.html', context=context)
+
+
+# @user_passes_test(lambda x: x.is_superuser)
+# def user_delete(request, user_pk):
+#     user = get_object_or_404(get_user_model(), pk=user_pk)
+#     if not user.is_active or request.method == 'POST':
+#         if user.is_active:
+#             user.is_active = False
+#             user.save()
+#         return HttpResponseRedirect(reverse('adminapp:index'))
+#
+#     context = {
+#         'page_title': 'админка/пользователи/удаление',
+#         'object': user
+#     }
+#     return render(request, 'adminapp/user_delete.html', context=context)
 
 
 @user_passes_test(lambda x: x.is_superuser)
